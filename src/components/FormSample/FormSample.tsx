b91a81304b736cb31ea3f440c1771d11bb71e91d
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2023-04-13 17:52:21 -0300

    basic sample

--- src/components/FormSample/FormSample.tsx
+++ src/components/FormSample/FormSample.tsx
@@ -1,5 +1,48 @@
+import { useForm } from "react-hook-form";
+
 const FormSample = () => {
-  return <>Form Sample</>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+
+  const firstName = watch("firstName");
+  const lastName = watch("lastName");
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input
+          {...register("firstName", { required: "This is required" })}
+          placeholder="First Name"
+        />
+        <p>{firstName}</p>
+        <p>{errors.firstName?.message}</p>
+        <input
+          {...register("lastName", {
+            required: "This is requiered",
+            minLength: { value: 4, message: "Min length is 4" },
+          })}
+          placeholder="Last Name"
+        />
+        <p>{lastName}</p>
+        <p>{errors.lastName?.message}</p>
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default FormSample;
